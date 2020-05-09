556eea1bedb417fa7885c48b08f4c67dca3ba65e
Rendi Kasigi <rkasigi@gmail.com>
2020-05-10 01:38:54 +0700

    donation: add migration and model

---
+++ app/Model/Donation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Donation extends Model
+{
+    protected $table = 'donations';
+    protected $primaryKey = 'donation_id';
+}
